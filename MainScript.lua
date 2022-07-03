6124f0714c0cd648af28f85e126940c2e3cb676e
randomdude <DoiGaMay@gmail.com>
2022-07-03 22:50:45 +0700

    e

--- MainScript.lua
+++ MainScript.lua
@@ -1,1 +1,7 @@
+--// Wait Until game is loaded
+repeat task.wait() until game:IsLoaded() == true
 
+local betterisfile = function(file)
+	local suc, res = pcall(function() return readfile(file) end)
+	return suc and res ~= nil
+end
